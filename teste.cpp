4b2f00fa645ad8bf146622829006e79a7675c386
Itzshey <sofiahelene1403@gmail.com>
2025-05-29 16:04:33 -0300

    Criacao do Menu e class Estudos

--- teste.cpp
+++ teste.cpp
@@ -1,12 +1,51 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <ctime>
 
 using namespace std;
 
+class Estudos 
+{
+    public:
+    string nomeDoAssunto;
+    string tempoInicio;
+    string tempoFim;
+};
+
+void menu() 
+{
+    cout << "[1] Nova sessão de estudos \n";
+    cout << "[2] Visualizar estudos anteriores \n";
+    cout << "[3] Sair do programa \n";
+}
+
 int main()
 {
-    cout << "Hello World";
-    return 0;
-    
+   menu();
+   cout << "Insira sua opção: ";
+   string x;
+   cin >> x;
+
+   while (x != "3")
+   {
+        if(x == "1")
+        {
+            cout << "Teste opção 1 \n";
+
+        } else if (x == "2")
+        {
+             cout << "Teste opção 2 \n";
+
+        } else
+        {
+             cout << "Teste input inválido \n";
+        }
+
+        menu();
+        cout << "Insira sua opção: ";
+        cin >> x;
+   }
+   
+
 }
