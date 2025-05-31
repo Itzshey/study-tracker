7f8acf38ea01f6412a85019a2e4df9c11555720e
Itzshey <sofiahelene1403@gmail.com>
2025-05-30 22:33:58 -0300

    Função de criar estudos, e visualizar estudos anteriores

---
+++ Estudos.hpp
@@ -0,0 +1,13 @@
+#ifndef ESTUDOS_H
+#define ESTUDOS_H
+
+#include <string>
+
+class Estudos {
+public:
+    std::string nomeEstudo;
+    std::string inicioEstudo;
+    std::string fimEstudo;
+};
+
+#endif
--- StudyTracker.cpp
+++ StudyTracker.cpp
@@ -3,9 +3,10 @@
 #include <string>
 #include <ctime>
 #include <limits>
+#include "Estudos.hpp"
+#include <fstream>
 
 using namespace std;
-
 
 void menu() 
 {
@@ -24,11 +25,68 @@ int obterOpcao()
     {
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        
     }
 
     return x;
 }
+
+void novoEstudo()
+{
+        Estudos nvEstudo;
+        cout << "Insira o nome do seu novo estudo: \n";
+        string nome;
+        cin >> nome;
+        nvEstudo.nomeEstudo = nome;
+
+        time_t timestamp = time(nullptr);
+        nvEstudo.inicioEstudo = ctime(&timestamp);
+
+        cout << "Para encerrar sua sessão de estudos aperte qualquer tecla e depois aperte enter. \n" ;
+        string n;
+        cin >> n;
+
+        time_t timestamp2 = time(nullptr);
+        nvEstudo.fimEstudo = ctime(&timestamp2);
+       
+       cout << "Salvando estudo... \n";
+
+       fstream myFile;
+       myFile.open("study-tracker.txt", ios::app); // PARA ESCREVER!!
+
+       if (myFile.is_open())
+       {
+            myFile << "\n Assunto: ";
+            myFile << nvEstudo.nomeEstudo;
+            myFile << "Início: ";
+            myFile << nvEstudo.inicioEstudo;
+            myFile << "Fim: \n";
+            myFile << nvEstudo.fimEstudo;
+            myFile.close();
+       }
+     
+       cout << "Sessão de estudos finalizada! \n";
+
+       
+}
+
+void estudosAnteriores()
+{
+    fstream myFile;
+    myFile.open("study-tracker.txt", ios::in);
+
+    if (myFile.is_open())
+    {
+        string line;
+        while (getline(myFile, line))
+        {
+            cout << line << endl;
+        }
+        
+        myFile.close();
+    }
+    
+}
+
 
 int main()
 {
@@ -40,15 +98,15 @@ int main()
         switch (x)
         {
         case 1:
-             cout << "Teste opção 2 \n";
+             novoEstudo();
             break;
 
         case 2:
-             cout << "Teste opção 2 \n";
+             estudosAnteriores();
             break;
         
         default:
-             cout << "Teste input inválido \n";
+             cout << "Teste input inválido. Insira um número das opções. \n";
             break;
         }
 
@@ -57,4 +115,7 @@ int main()
 
    }
 
+   cout << "Salvando informações... \n";
+   cout << "Até a próxima sessão de estudos!\n";
+   
 }
